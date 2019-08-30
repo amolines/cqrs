558d38ed120ec05a1bc8e7c3b739b234d7766968
Alejandro Molines Hurtado <alejandro.molines@mecalux.com>
2019-08-30 18:14:38 +0200

    control version aggregate in MessageBroker

--- src/Xendor/MessageBroker/IEnvelope.cs
+++ src/Xendor/MessageBroker/IEnvelope.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using Xendor.Data;
 
 namespace Xendor.MessageBroker
 {
-    public interface IEnvelope 
+    public interface IVersion
     {
-         Guid AggregateId { get; }
-         int Version { get; }
-         long TimeStamp { get; }
-         IDictionary<string, object> Payload { get; }
-         string ContentType { get; }
+        Guid AggregateId { get; }
+        int Version { get; }
+        string ContentType { get; }
 
+    }
+
+    public interface IAggregateService
+    {
+        int CurrentVersion(IVersion version);
+        void ChangeVersion(IVersion version);
+    }
+
+    public class AggregateService : IAggregateService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public AggregateService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+
+        public int CurrentVersion(IVersion version)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeVersion(IVersion version)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public interface IEnvelope : IVersion
+    {
+        long TimeStamp { get; }
+         IDictionary<string, object> Payload { get; }
     }
 }
